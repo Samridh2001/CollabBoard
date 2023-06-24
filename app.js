3330e46ed6cb27793cf971dc4780e61c60ee8987
Samridh2001 <109023430+Samridh2001@users.noreply.github.com>
2023-06-24 23:53:24 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const userRouter = require("./routes/userRouter");
+const boardRouter = require("./routes/boardRouter");
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// REACT BUILD for production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "build")));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+  });
+}
+
+//ROUTES
+app.use("/api/auth", userRouter);
+app.use("/api/board", boardRouter);
+
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
+
+module.exports = app;
